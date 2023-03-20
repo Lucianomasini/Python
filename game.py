f621f600afba2d5beae417b9b69afa8d961243d3
Luciano Elian Masini <lucianomasini17@gmail.com>
2023-03-19 21:16:22 -0300

    Ahora anda bien

--- game.py
+++ game.py
@@ -13,12 +13,12 @@ for i in range(0, times):
 # Se eligen números y operador al azar
     number_1 = randrange(10)
     number_2 = randrange(10)
-operator = choice(operators)
-1
+    operator = choice(operators)
+    
 # Se imprime la cuenta.
-print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
 # Le pedimos al usuario el resultado
-result = input("resultado: ")
+    result = input("resultado: ")
 # Al terminar toda la cantidad de cuentas por resolver.
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()

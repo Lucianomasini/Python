fd6483d8dedfa1e03a992b39ef315e90452d7a9f
Luciano Elian Masini <lucianomasini17@gmail.com>
2023-03-19 22:38:17 -0300

    Objetivos cumplidos

--- game.py
+++ game.py
@@ -2,13 +2,14 @@ from random import choice, randrange
 from datetime import datetime
 
 # Operadores posibles
-operators = ["+", "-"]
+operators = ["+", "-", "*", "/"]
 # Cantidad de cuentas a resolver
 times = 5
 # Contador inicial de tiempo.
 # Esto toma la fecha y hora actual.
 init_time = datetime.now()
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+print(f"Recuerde escribir el resultado de la división solo con 3 decimales en caso de ser necesario")
 for i in range(0, times):
 # Se eligen números y operador al azar
     number_1 = randrange(10)
@@ -18,7 +19,23 @@ for i in range(0, times):
 # Se imprime la cuenta.
     print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
 # Le pedimos al usuario el resultado
-    result = input("resultado: ")
+    result = input()
+    resultado = float(result)
+# Le pedimos que nos diga si está bien o mal
+    if operator == "+" and (resultado == (number_1 + number_2)):
+        print(f"El resultado esta bien")
+    elif operator == "-" and (resultado == (number_1 - number_2)):
+        print(f"El resultado esta bien")
+    elif operator == "*" and (resultado == (number_1 * number_2)):
+        print(f"El resultado esta bien")
+    elif operator == "/" and (not number_2 == 0) and (resultado == round((number_1 / number_2), 3)):
+        print(f"El resultado esta bien")
+    else:
+        if ((operator == "/") and (number_2 == 0)):
+            print(f"La división por cero no está definida")
+        else:
+            print(f"El resultado esta mal")
+
 # Al terminar toda la cantidad de cuentas por resolver.
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()
